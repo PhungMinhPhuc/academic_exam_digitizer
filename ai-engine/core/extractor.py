aa4a245858b7ea5564c457a935ab4f25273e92ea
PhungMinhPhuc <phungminhphuc28092006@gmail.com>
2026-03-12 15:40:53 +0700

    New features: layout_analyzer and graphic_engine

--- ai-engine/core/extractor.py
+++ ai-engine/core/extractor.py
@@ -2,15 +2,25 @@ import pymupdf
 import os
 
 OUTPUT = "figure_folder"
+pdf_path = "E:\Downloads\Visionary_Solutions_for_Academic_Digitization\Test_sample\Test_T_2018.pdf"
 
-def imgExtraction(PDF_PATH, prompt):
+def imgExtraction(PDF_PATH):
     if not os.path.exists(OUTPUT): os.makedirs(OUTPUT)
     doc = pymupdf.open(PDF_PATH)
-    fig_count = 0
+    figCount = 0
     for page in doc:
-        # from  in page.get_images()
+        for img in page.get_images():
+            xref = img[0] # cross reference - get image ID in the pdf file
+            pix = pymupdf.Pixmap(doc, xref) # convert the image object to proccessable image data
+            if pix.n - pix.alpha > 3: pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
+            pix.save(os.path.join(OUTPUT, f"figure_{figCount + 1}.png"))
+            figCount += 1
+    return figCount
 
-    
+if __name__ == "__main__":
+    imgExtraction(pdf_path)
+
+
 # def extract_images():
 # if not os.path.exists(IMAGE_FOLDER): os.makedirs(IMAGE_FOLDER)
 # doc = fitz.open(PDF_PATH)
